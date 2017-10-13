ac86b068d3d2fc2083d8fbc602717a479c0b7786
saoirsekearse <s.kearse2@nuigalway.ie>
2017-10-13 15:28:12 +0100

    first commit

--- src/main/java/SutdentRegDriver/StudentRegDriver/Driver.java
+++ src/main/java/SutdentRegDriver/StudentRegDriver/Driver.java
@@ -1,56 +1,94 @@
 package SutdentRegDriver.StudentRegDriver;
 
-import java.util.ArrayList;
 import StudentRegAssignment.Course;
 import StudentRegAssignment.Student;
 import StudentRegAssignment.Subjects;
 
-import org.joda.time.DateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.joda.time.LocalDate;
 
 
 
 public class Driver {
 
-    static ArrayList<Subjects> ECESubject = new ArrayList<Subjects>();
-	static ArrayList<Subjects> CompSciSubject = new ArrayList<Subjects>();
    
-	static ArrayList<Student> ECEStudents = new ArrayList<Student>();
-    static ArrayList<Student> CompSciStudents = new ArrayList<Student>();
-    
-    static ArrayList<Student> students = new ArrayList<Student>();
-    
-    
     
     public static void main(String []args){
     	
-    	ECEStudents.add(new Student("Saoirse ","30/08/1997", 14417498));
-    	ECEStudents.add(new Student("James", "4/12/1995", 14312243));
-    	ECEStudents.add(new Student("Niamh", "11/09/1996", 14351432));
-    	CompSciStudents.add(new Student("Barry", "07/05/1996", 14214309));
-    	CompSciStudents.add(new Student("Sarah", "12/01/1995", 14513287));
-    	CompSciStudents.add(new Student("Sean", "01/09/1996", 14232589));
-    	
-        students.addAll(ECEStudents);
-        students.addAll(CompSciStudents);
+    	Student s1 = new Student("Saoirse ", "30/08/1997", 14417498);
+    	Student s2 = new Student("James", "4/12/1995", 14312243);
+    	Student s3 = new Student("Niamh", "11/09/1996", 14351432);
+    	Student s4 = new Student("Barry", "07/05/1996", 14214309);
+    	Student s5 = new Student("Sarah", "12/01/1995", 14513287);
+    	Student s6 = new Student("Sean", "01/09/1996", 14232589);
         
-        ECESubject.add(new Subjects("System on Chip"));
-        ECESubject.add(new Subjects("Digital Signal Processing"));
-        ECESubject.add(new Subjects("Tellocomunications"));
-        CompSciSubject.add(new Subjects("Software Engineering"));
-        CompSciSubject.add(new Subjects("Machine Learning"));
+    	
+    	
+    	Subjects sub1 = new Subjects("System on Chip");
+        Subjects sub2 = new Subjects("Digital Signal Processing");
+        Subjects sub3 = new Subjects("Telecomunications");
+        Subjects sub4 = new Subjects("Software Engineering");
+        Subjects sub5 = new Subjects("Machine Learning");
+       
+        sub1.addStudent(s1);
+        sub1.addStudent(s2);
+        sub1.addStudent(s3);
+      //  System.out.println("Students taking " + sub1 + ":\n"); 
+       // System.out.println(sub1.getStudent() + "\n");
+        
+        sub2.addStudent(s1);
+        sub2.addStudent(s2);
+        sub2.addStudent(s3);
+        sub2.addStudent(s4);
+       // System.out.println("Students taking " + sub2 + ":\n"); 
+       // System.out.println(sub2.getStudent() + "\n");
+        
+        sub3.addStudent(s1);
+        sub3.addStudent(s2);
+        sub3.addStudent(s3);
+        sub3.addStudent(s4);
+        sub3.addStudent(s5);
+        sub3.addStudent(s6);
+       // System.out.println("Students taking " + sub3 + ":\n"); 
+       // System.out.println(sub3.getStudent() + "\n");
+        
+        sub4.addStudent(s1);
+        sub4.addStudent(s2);
+        sub4.addStudent(s4);
+        sub4.addStudent(s5);
+        sub4.addStudent(s6);
+       // System.out.println("Students taking " + sub4 + ":\n"); 
+      //  System.out.println(sub4.getStudent() + "\n");
+        
+        sub5.addStudent(s2);
+        sub5.addStudent(s3);
+        sub5.addStudent(s4);
+        sub5.addStudent(s5);
+        sub5.addStudent(s6);
+        //System.out.println("Students taking " + sub5 + ":\n"); 
+        //System.out.println(sub5.getStudent() + "\n");
         
         Course ECE = new Course("Electronic and Computer Engineering", "BP", new LocalDate(2017,9,05), new LocalDate(2018,5,16));
-        ECE.setSubjects(ECESubject);
         
-        Course CompSci = new Course("Computer Science", "BCT", new LocalDate(2017,9,05), new LocalDate(2018,5,16));
-        CompSci.setSubjects(CompSciSubject);
+        ECE.addSubject(sub1);
+        ECE.addSubject(sub2);
+        ECE.addSubject(sub3);
+        ECE.addSubject(sub4);
+        ECE.addSubject(sub5);
         
-        for(Student student:students){
-        	
-        	System.out.println("Student details :\n" + student + "\n Course :" + student.getCourse() + "\n Subjects :" + student.getSubjects());
        
+       System.out.println("Course:\n"); 
+       System.out.println(ECE + "\n");
+       System.out.println("Subjects in course:\n"); 
+       System.out.println(ECE.getSubjects() + "\n");
+       System.out.println("Students Registered:\n"); 
+       System.out.println(ECE.getStudents() + "\n");
+
+        for (Student i : ECE.getStudents()){
+            System.out.println(i.getStudentUsername());
+        	
         }
-        
     }
 }
